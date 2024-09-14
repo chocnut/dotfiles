1ae5f0913b4cceaf5fca29a458d0d4edecd51ae1
Peter Indiola <pindiola@gmail.com>
2024-09-14 10:54:50 +0800

    added plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+
     { import = "plugins" },
   },
   defaults = {
